bef906c6ebf583256d77c18ba58bbfcbf729546e
zain0011 <40404880+zain0011@users.noreply.github.com>
2018-10-15 17:19:53 +0300

    Create do3.js

---
+++ do3.js
@@ -0,0 +1,214 @@
+var arr = [
+	{
+		name: ' آنہۣۗہۧاقہۣۗہه آنہۣۗثہۧى\'ۦ \'ۦ، '
+		, age: 18
+		, img: 'https://galychat.com/dro3/z1cdofo4ft10.gif'
+		, icoimg: 'http://galychat.com/sico/z1cprqeps110.jpg'
+		, ico: 'iq'
+		, info: '😔 آلقلۆب آلطيبهہَ مظلـوُمهہَ دآئما ، لآنهَا آسّرعْ مـٓن يفَتّح الآبوُآبْ ، وَ آوُل من يتلقّى آلضّرَبآتْ .. ! '
+		, id: '9'
+}
+
+	, {
+		name: ' ڒٍوُڒٍوُ (̲̅z̲̅)(̲̅o̲̅)(̲̅z̲̅)(̲̅o̲̅)'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cprud8ln10.jpg'
+		, ico: 'iq'
+		, info: 'لَدِيِّنِأّ تّأّريِّخَ #أّذّأّ ذّګر#يِّمََّسحٌ مَأّضّيِّګ الشمس🌞 والقمر🌕 كوكبان✌وكلام🔇 الناس👥 وحذائي👞 توؤمان✌ فديته'
+		, id: '9'
+}
+
+	, {
+		name: ' αиσɒɪн ﹏ √ عٌـِـِِـِـنـِِـِــــــــوُڊيــــه﹏'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cprucec110.jpg'
+		, ico: 'us'
+		, info: '　　　　　　　　　 \'　　　 　　　　　　　　　　　　　　　　　　　　 　　　　　　　　　　　　　　　　 　　　　　　　　　　　　 　　 　　 　　　　　　　　　　　　　آنمآ آلُِعٍيوُن تحٍڪي مآ لُِآ يقٌآل'
+		, id: '9'
+}
+
+	, {
+		name: ' ❥↝❰ نـِِـِـوُر آلُـِـِِـِِِـِِـِـڛـ,ـمـْـْْـْآء ❱✿ 🎀'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cpruchdh10.jpg'
+		, ico: 'kw'
+		, info: 'بلاء الإنسان من اللسان'
+		, id: '9'
+}
+
+	, {
+		name: ' عــراقيــه'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cpruckkk10.jpg'
+		, ico: 'iq'
+		, info: 'اني لا ذهب ولا ماس اني عراقيه رفعة راس  '
+		, id: '9'
+}
+
+	, {
+		name: ' 　 ✺ ＡＳＨＱ┊ عــــٰٰٖـــٰٰٖــشــــٰٰٖـقِ ✺'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'sico/z1cpi67itf10.jpg'
+		, ico: 'gb'
+		, info: 'ظحكاتي لو كلهن صدك جاصرت رب العافيه '
+		, id: '9'
+}
+
+	, {
+		name: ' ༒　مـًـ’ٰـۦــخٌـۦـاّٰإْوِِيـ،\'ٰٓــۦــة اِْلـۦــ\'ًـــذٰيـــ،ّــۦـب '
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'sico/z1cpi6g9p710.jpg'
+		, ico: 'iq'
+		, info: '　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　مّــ،ـــآإْخٰــ’ـــاٰإوــي’آلــٰ’ـــرخـ،ٍٰـــاآصـ　　　　　　　　　　　　　　　　　　　　　　　　 '
+		, id: '9'
+}
+
+	, {
+		name: ' روحـ♚ـٰہ تــ͡ــٍُِِعــ͡ــٍُِِشــ͡ــٍُِِق الــ͡ــٍُِِتمــ͡ــٍُِِييــ͡ــٍُِِز'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'sico/z1cpi6egn310.jpg'
+		, ico: 'sa'
+		, info: 'بلاء .. تعبتـج يَ روحـي ويـاي '
+		, id: '9'
+}
+];
+jQuery['each'](arr, function (_0xe3f4x2, _0xe3f4x3) {
+	for (var _0xe3f4x2 = 0, _0xe3f4x4 = 1; _0xe3f4x2 < _0xe3f4x4; _0xe3f4x2++) {
+		$(' <div class="hand nosel fl uzr border uid1cf9ga0ms10 inroom" style="text-align: left; background-color: white; width: 99%; padding: 1px; border-radius: 0px; margin: 1px 0px; border-width: 1px; border-style: solid; border-color: rgb(68, 152, 224);" v="' + _0xe3f4x3['id'] + '">\x0D\x0A      <img class="fl ustat" style="width:3px;height:36px;margin-left: 1px;" src="' + _0xe3f4x3['img'] + 'imgs/s2.png?2">\x0D\x0A      <img style="width: 36px; height: 36px; margin-left: 1px; background-image: url(\'' + _0xe3f4x3['icoimg'] + '\');" class="fitimg fl u-pic ">\x0D\x0A      <label class="fl muted fa" style="color:indianred"></label>\x0D\x0A	  <img alt="" class="fr co" style="width:16px;border-radius:1px;" src="' + _0xe3f4x3['img'] + 'flag/' + _0xe3f4x3['ico'] + '.gif"><div style="width:72%;" class="fl">\x0D\x0A        <div style="width:100%;margin-top:-2px;margin-left: 3px;" class="fl">\x0D\x0A          &nbsp;<img class="fl u-ico" alt="" src="' + _0xe3f4x3['img'] + '">&nbsp;\x0D\x0A          <div class="fl" style="width:82%;"><span style="margin-top: -2px;margin-top: 1px; padding: 0px 8px; max-width: 100%; border-radius: 3px; color: rgb(0, 0, 0);" class="corner u-topic dots">' + _0xe3f4x3['name'] + '</span></div>\x0D\x0A        </div>\x0D\x0A        <div style="width:100%;color:#888;margin-top:-8px;margin-top:-2px;margin-left: 3px;" class="fl mini u-msg">' + _0xe3f4x3['info'] + '</div>\x0D\x0A      </div>\x0D\x0A    </div>')['appendTo']('#users')
+	}
+});
+var arr = [
+	{
+		name: ' FLAMiNGO .'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cprucq1d10.jpg'
+		, ico: 'ae'
+		, info: 'بلاء .. تعبتـج يَ روحـي ويـاي '
+		, id: '9'
+}
+
+	, {
+		name: ' ٱلَنٌےـسًےـر ٱلَذٌهےـِبّےـ ـيّےـ'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cprudful10.jpg'
+		, ico: 'nl'
+		, info: '…'
+		, id: '9'
+}
+
+	, {
+		name: ' امــہيــہرٰ اٰلــہعـہرٰاٰقي'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cpruc9i610.jpg'
+		, ico: 'iq'
+		, info: ' (عضو جديد) '
+		, id: '9'
+}
+];
+jQuery['each'](arr, function (_0xe3f4x2, _0xe3f4x3) {
+	for (var _0xe3f4x2 = 0, _0xe3f4x4 = 1; _0xe3f4x2 < _0xe3f4x4; _0xe3f4x2++) {
+		$(' <div class="hand nosel fl uzr border uid1cf9ga0ms10 inroom" style="text-align: left; background-color: white; width: 99%; padding: 1px; border-radius: 0px; margin: 1px 0px; border-width: 1px; border-style: solid; border-color: rgb(68, 152, 224);" v="' + _0xe3f4x3['id'] + '">\x0D\x0A      <img class="fl ustat" style="width:3px;height:36px;margin-left: 1px;" src="' + _0xe3f4x3['img'] + 'imgs/s0.png?2">\x0D\x0A      <img style="width: 36px; height: 36px; margin-left: 1px; background-image: url(\'' + _0xe3f4x3['icoimg'] + '\');" class="fitimg fl u-pic ">\x0D\x0A      <label class="fl muted fa" style="color:indianred"></label>\x0D\x0A	  <img alt="" class="fr co" style="width:16px;border-radius:1px;" src="' + _0xe3f4x3['img'] + 'flag/' + _0xe3f4x3['ico'] + '.gif"><div style="width:72%;" class="fl">\x0D\x0A        <div style="width:100%;margin-top:-2px;margin-left: 3px;" class="fl">\x0D\x0A          &nbsp;<img class="fl u-ico" alt="" src="' + _0xe3f4x3['img'] + '">&nbsp;\x0D\x0A          <div class="fl" style="width:82%;"><span style="margin-top: -2px;margin-top: 1px; padding: 0px 8px; max-width: 100%; border-radius: 3px; color: rgb(0, 0, 0);" class="corner u-topic dots">' + _0xe3f4x3['name'] + '</span></div>\x0D\x0A        </div>\x0D\x0A        <div style="width:100%;color:#888;margin-top:-8px;margin-top:-2px;margin-left: 3px;" class="fl mini u-msg">' + _0xe3f4x3['info'] + '</div>\x0D\x0A      </div>\x0D\x0A    </div>')['appendTo']('#users')
+	}
+});
+var arr = [
+	{
+		name: '  \u1613\u0279\u0308\u0279\u0324\u1397\u14D7\u0E3A\u020A\u0261 \u14AA\u1397\u0DB6\u0279\u0308\u0279\u0307\u14C5\u2184\xA0'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cprucnee10.jpg'
+		, ico: 'it'
+		, info: 'اذا لم تسطيع اصلاح نفسك حاول افساد الاخرين '
+		, id: '9'
+}
+
+	, {
+		name: ' w\uD835\uDDA5\uD835\uDDA0\uD835\uDDA4\uD835\uDDA8 \uD835\uDDA0\uD835\uDDADw\uD835\uDDA0\uD835\uDDAD\uD835\uDDA8\u0648\u064F\u0641\u064F\u0622\u0626\u064A \u0639\u064D\u0646\u0648\u064F\u0622\u0646\xA0'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cprucnee10.jpg'
+		, ico: 'iq'
+		, info: 'و يكفينا من الحياة .. 🌼🍃 لحظة حب أحسسناها بصدق مرة واحدة .. و لو ذهبت غير آتية 🌸 '
+		, id: '9'
+}
+
+	, {
+		name: ' \u22C6\u2605 \u0633\u0640\u0640\u0670\u0670\u0670\u0670\u06EA\u0656\u06EB\u0640\u0670\u0640\u0670\u0670\u06EB\u0640\uFBAF\u0337\u0361\uFBA9\u062A \u0622\u0644\u062D\u0633\u0640\u0670\u0670\u0670\u0670\u06EA\u0656\u06EB\u0640\u0670\u0640\u0670\u0670\u06EB\u0640\uFBAF\u0337\u0361\uFBA9\u0640\u0646 \u2605\u22C6\xA0'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'http://galychat.com/sico/z1cprqeps110.jpg'
+		, ico: 'ir'
+		, info: 'أُإنہأُإ ضًدٍ ڱل ڜيّ يّأُإخُذٍڱ مہنہيّ .. أُإيّيّيّ حّتًى نہوُمہڱ  '
+		, id: '9'
+}
+
+	, {
+		name: ' \u25C9\xA0\u06F0\u06EA\u06EBR\u06EA\u06EB\u06F0\u06F0\u06EA\u06EBA\u06EA\u06EB\u06F0\u06F0\u06EA\u06EBM\u06EA\u06EB\u06F0\u06F0\u06EA\u06EBY\u06EA\u06EB\u06F0 \u0695\u0627\u0645\u0640\u0640\u06D0\u0652 \u25C9\xA0'
+		, age: 18
+		, img: 'https://iqr30.com/'
+		, icoimg: 'http://galychat.com/sico/z1cprqeps110.jpg'
+		, ico: 'eg'
+		, info: 'لو كانت الحياة وردة، لنجح الجميع باستنشاق رحيقها. '
+		, id: '9'
+}
+
+	, {
+		name: ' \u2740 \u0336s\u0336\xA0\u0336i\u0336\xA0\u0336d\u0336\xA0\u0336r\u0336\xA0\u0336i\u0336 \u2551\u0633\u0640\u06C1\u0640\u06E6\u06E6\u06E6\u0640\u062F\uFBE0 \u2740'
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cps14k8010.png'
+		, ico: 'iq'
+		, info: '😋😋م: التاريخ لا يُعيد نفسه، بل نحن مَن نواصل إعادتهُ وتكرار نفس الأخطاء حتّى مَلَّ منا.ع. '
+		, id: '9'
+}
+];
+jQuery['each'](arr, function (_0xe3f4x2, _0xe3f4x3) {
+	for (var _0xe3f4x2 = 0, _0xe3f4x4 = 1; _0xe3f4x2 < _0xe3f4x4; _0xe3f4x2++) {
+		$(' <div class="hand nosel fl uzr border uid1cf9ga0ms10 inroom" style="text-align: left; background-color: white; width: 99%; padding: 1px; border-radius: 0px; margin: 1px 0px; border-width: 1px; border-style: solid; border-color: rgb(68, 152, 224);" v="' + _0xe3f4x3['id'] + '">\x0D\x0A      <img class="fl ustat" style="width:3px;height:36px;margin-left: 1px;" src="' + _0xe3f4x3['img'] + 'imgs/s1.png?2">\x0D\x0A      <img style="width: 36px; height: 36px; margin-left: 1px; background-image: url(\'' + _0xe3f4x3['icoimg'] + '\');" class="fitimg fl u-pic ">\x0D\x0A      <label class="fl muted fa" style="color:indianred"></label>\x0D\x0A	  <img alt="" class="fr co" style="width:16px;border-radius:1px;" src="' + _0xe3f4x3['img'] + 'flag/' + _0xe3f4x3['ico'] + '.gif"><div style="width:72%;" class="fl">\x0D\x0A        <div style="width:100%;margin-top:-2px;margin-left: 3px;" class="fl">\x0D\x0A          &nbsp;<img class="fl u-ico" alt="" src="' + _0xe3f4x3['img'] + '">&nbsp;\x0D\x0A          <div class="fl" style="width:82%;"><span style="margin-top: -2px;margin-top: 1px; padding: 0px 8px; max-width: 100%; border-radius: 3px; color: rgb(0, 0, 0);" class="corner u-topic dots">' + _0xe3f4x3['name'] + '</span></div>\x0D\x0A        </div>\x0D\x0A        <div style="width:100%;color:#888;margin-top:-8px;margin-top:-2px;margin-left: 3px;" class="fl mini u-msg">' + _0xe3f4x3['info'] + '</div>\x0D\x0A      </div>\x0D\x0A    </div>')['appendTo']('#users')
+	}
+});
+var arr = [
+	{
+		name: ' 		 ║▓ ♚ بٰٰـــصہٰـــراويٰـــةه واحہٰـــجـــيٰ مـــعـــلہٰـــكہٍّْٰ ♚ ▓║ '
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cps17lae10.png'
+		, ico: 'iq'
+		, info: 'لدي اُسلوب استفزازي يجعلك تكرهني💔 ولدي حب،💗💗💗،،،،،،،،،،،،،،،،،،،،،،،،،،، لا تجده في احد غيري فتعلم كيف تعاملني😉 '
+		, id: '9'
+}
+
+	, {
+		name: '		 ديـ.ًِ/ـآ‘إْن.َُِ‘ـُّّآ‘إْ آ‘إْلع.َُِ‘ـُّّـ{ب}ـُّّيدي  '
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/sico/z1cps1fq2r10.jpg'
+		, ico: 'ae'
+		, info: 'لا تقهر احد كي تسعد نفسك 😶.ولاتظلم نفسا لكي تبرر اخطائك 😯 حاول دائما ان تبني سعادتك 😊 بعيد عن اذية الناس ❤️ '
+		, id: '9'
+}
+
+	, {
+		name: '		 ᘓᓆl͠ȷ᎗Ըᒧl͠ ɑ̈᎗Ըɡȷ ❥  '
+		, age: 18
+		, img: 'https://galychat.com/'
+		, icoimg: 'https://galychat.com/'
+		, ico: ''
+		, info: 'جمال العقل بالفكر●وجمال القلب بالأخلاق● وجمال اللسان بالصمت ●وجمال الوجه بالابتسامه'
+		, id: '9'
+}
+];
+jQuery['each'](arr, function (_0xe3f4x2, _0xe3f4x3) {
+	for (var _0xe3f4x2 = 0, _0xe3f4x4 = 1; _0xe3f4x2 < _0xe3f4x4; _0xe3f4x2++) {
+		$(' <div class="hand nosel fl uzr border uid1cf9ga0ms10 inroom" style="text-align: left; background-color: white; width: 99%; padding: 1px; border-radius: 0px; margin: 1px 0px; border-width: 1px; border-style: solid; border-color: rgb(68, 152, 224);" v="' + _0xe3f4x3['id'] + '">\x0D\x0A      <img class="fl ustat" style="width:3px;height:36px;margin-left: 1px;" src="' + _0xe3f4x3['img'] + 'imgs/s2.png?2">\x0D\x0A      <img style="width: 36px; height: 36px; margin-left: 1px; background-image: url(\'' + _0xe3f4x3['icoimg'] + '\');" class="fitimg fl u-pic ">\x0D\x0A      <label class="fl muted fa" style="color:indianred"></label>\x0D\x0A	  <img alt="" class="fr co" style="width:16px;border-radius:1px;" src="' + _0xe3f4x3['img'] + 'flag/' + _0xe3f4x3['ico'] + '.gif"><div style="width:72%;" class="fl">\x0D\x0A        <div style="width:100%;margin-top:-2px;margin-left: 3px;" class="fl">\x0D\x0A          &nbsp;<img class="fl u-ico" alt="" src="' + _0xe3f4x3['img'] + 'sico/z1coj9ee0h10.gif">&nbsp;\x0D\x0A          <div class="fl" style="width:82%;"><span style="margin-top: -2px;margin-top: 1px; padding: 0px 8px; max-width: 100%; border-radius: 3px; color: rgb(0, 0, 0);" class="corner u-topic dots">' + _0xe3f4x3['name'] + '</span></div>\x0D\x0A        </div>\x0D\x0A        <div style="width:100%;color:#888;margin-top:-8px;margin-top:-2px;margin-left: 3px;" class="fl mini u-msg">' + _0xe3f4x3['info'] + '</div>\x0D\x0A      </div>\x0D\x0A    </div>')['appendTo']('#users')
+	}
+})
